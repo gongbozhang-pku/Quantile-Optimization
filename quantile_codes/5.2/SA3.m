d3dd3b14f07440508f3dfe770a0802e6316bbcbf
gongbozhang-pku <48860844+gongbozhang-pku@users.noreply.github.com>
2022-03-10 19:57:38 +0800

    Update SA3.m

--- quantile_codes/5.2/SA3.m
+++ quantile_codes/5.2/SA3.m
@@ -2,7 +2,7 @@ function [Y]=SA3(alpha,x,d,q,a1,a2,a3,b1,b2,b3,K,p,c)
 Y=zeros(1,K);
 for k=1:K
     Y(k) = x;
-    R=gprnd(2,2,1,[1,1]);
+    R=gprnd(1,1,1,[1,1]);
     RR=log(R-1);
     Z=p*min(x,RR) - c*x;
     d=d+b1*(((p-c)*x)>q)*(q >= (p*RR-c*x))/p*(1-2/3*exp(RR)/(exp(RR)+1))*(-c-d)/k^a1;
@@ -14,10 +14,10 @@ for k=1:K
         q = 10^10;
     end
     
-    if x < 400
-        x = 400;
-    elseif x > 800
-        x = 800;
+    if x < 20
+        x = 20;
+    elseif x > 80
+        x = 80;
     end
 end
 end
