47b83f66135f385f401e7476698d1c97bff5bd0c
gongbozhang-pku <48860844+gongbozhang-pku@users.noreply.github.com>
2022-03-10 19:58:19 +0800

    Update signum.m

--- quantile_codes/5.2/signum.m
+++ quantile_codes/5.2/signum.m
@@ -1,6 +1,6 @@
 function [Y] = signum(alpha,x,K,cc,p,c)
 Y = zeros(1,K);
-R = gprnd(2,2,1,[1,10]);
+R = gprnd(1,1,1,[1,10]);
 Z = p*min(x,R) - c*x; 
 [B,~] = sort(Z);
 Bb = B(floor(10*0.75)+1);
@@ -8,17 +8,17 @@ Bs = B(floor(10*0.25)+1);
 sigma = (Bb-Bs)/2;
 for k=1:K
     Y(:,k)=x;
-    R  = gprnd(2,2,1,[1,1]);
+    R  = gprnd(1,1,1,[1,1]);
     if x <= R
         x = x + cc*(alpha)*sigma/k;
     else
         x = x - cc*(1 - alpha)*sigma/k;
     end    
-    if x < 400
-        x = 400;
+    if x < 20
+        x = 20;
     end
-    if x > 800
-        x = 800;
+    if x > 80
+        x = 80;
     end
 end
 end
